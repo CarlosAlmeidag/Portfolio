df6fa48d421248fd91f8ef10aab5b250a7cc442c
Carlos Almeida <carlosgarcia53105@gmail.com>
2026-03-23 14:36:46 -0300

    Atualizando templates

--- src/main/java/com/portfolio/Carlos/Controller/AdminController.java
+++ src/main/java/com/portfolio/Carlos/Controller/AdminController.java
@@ -1,15 +1,28 @@
 package com.portfolio.Carlos.Controller;
 
+import com.portfolio.Carlos.repository.PerfilRepository;
+import com.portfolio.Carlos.repository.ProjetoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    @Autowired
+    private PerfilRepository perfilRepository;
+
+    @Autowired
+    private ProjetoRepository projetoRepository;
+
     @GetMapping
-    public String admin() {
-        return "admin";
+    public String admin(Model model) {
+        model.addAttribute("perfis", perfilRepository.findAll());
+        model.addAttribute("projetos", projetoRepository.findAll());
+        return "admin/admin";
     }
 }
